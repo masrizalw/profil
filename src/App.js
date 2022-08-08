2c5150748a9090de2b64a3b14e0d42687db80526
masrizalw <masrizalw@gmail.com>
2022-08-08 14:21:49 +0700

    g paham

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hadeh error njir</h1>
+    <div className="container">
+      <header className="row">
+        <h1>undeploy h1</h1>
       </header>
     </div>
   );
