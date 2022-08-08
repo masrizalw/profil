fe8b39b3d1229891a7c3324872cb9c0a910c0550
masrizalw <masrizalw@gmail.com>
2022-08-08 14:48:20 +0700

    try to upload cname

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="container">
-      <header className="row">
-        <h1>undeploy h1</h1>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-3'><h1>Hello</h1></div>
+            <div className='col-3'><h1>I'M</h1></div>
+            <div className='col-3'><h1>Mr. W</h1></div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
